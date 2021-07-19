d7d66a080ecde2d150a620855c95ab7b36a0fd7a
Truongvanlong25102000 <42183031+Truongvanlong25102000@users.noreply.github.com>
2021-07-19 08:47:01 +0700

    update

--- app/src/main/java/com/example/messenger/activites/MainActivity.java
+++ app/src/main/java/com/example/messenger/activites/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
     private NavHostFragment navHostFragment;
     private NavController navController;
     private TextView banner_txt_count_message;
-    private ImageView banner_img_search;
+    private ImageView banner_img_search,banner_img_edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +44,10 @@ public class MainActivity extends AppCompatActivity {
         banner_img_search.setOnClickListener(v -> {
             startActivity(new Intent(MainActivity.this, SearchActivity.class));
         });
+
+        banner_img_edit.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, SearchActivity.class));
+        });
     }
 
     private void initView() {
@@ -54,5 +58,6 @@ public class MainActivity extends AppCompatActivity {
         //kotlin bottom_nav.setupWithNavController(navController);
         banner_txt_count_message = findViewById(R.id.banner_txt_count_message);
         banner_img_search = findViewById(R.id.banner_img_search);
+        banner_img_edit = findViewById(R.id.banner_img_edit);
     }
 }
---
+++ app/src/main/java/com/example/messenger/activites/create_message/CreateMessageActivity.java
@@ -0,0 +1,16 @@
+package com.example.messenger.activites.create_message;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.messenger.R;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+}
